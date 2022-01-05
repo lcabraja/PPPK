a5bde11f2ba282d7e3baf510d0d9be1e97050264
Luka Cabraja <luka@cabraja.eu>
2022-01-05 15:06:50 +0100

    small commit

---
+++ RemoteFileManager/RemoteFileManager/Dao/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteFileManager.Dao
+{
+    static class Repository
+    {
+        private const string ContainerName = "blobcontainer";
+        private static readonly string cs = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+
+        private static readonly Lazy<>
+    }
+}
