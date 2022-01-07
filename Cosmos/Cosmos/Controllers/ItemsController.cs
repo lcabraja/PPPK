a45f854881222563d5ca3a6bcd7d8e0b5b99e129
Luka Cabraja <luka@cabraja.eu>
2022-01-07 14:20:18 +0100

    created controller

---
+++ Cosmos/Cosmos/Controllers/ItemsController.cs
@@ -0,0 +1,92 @@
+﻿using Cosmos.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cosmos.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public static readonly ICosmosDbService service = CosmosDbServiceProvider.CosmosDbService;
+        // GET: Items
+        public async Task<ActionResult> Index()
+        {
+            return View(await service.GetItemsAsync("SELECT * FROM Item"));
+        }
+
+        // GET: Items/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Items/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Items/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Items/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Items/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Items/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Items/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
