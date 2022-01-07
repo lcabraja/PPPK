c0d1a5119f3da65e1395b7bd33c7cb92680e9d22
Luka Cabraja <luka@cabraja.eu>
2022-01-07 13:25:23 +0100

    delivery-6 init

---
+++ Cosmos/Cosmos/Dao/CosmosDbServiceProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cosmos.Dao
+{
+    public class CosmosDbServiceProvider
+    {
+
+        private const string Account = "https://todoitems.documents.azure.com:443/";
+        private const string Key = "ozlT91aEkjdOhBBy6Jzhx5X8NdB3CP9OqNyhhbjrlSlU0vAxaEVw2eaxceFHdRILZQy7IUo0aZvknmm76w23IA==";
+        private const string ContainerName = "Tasks";
+        private const string DatabaseName = "Tasks";
+    }
+}
