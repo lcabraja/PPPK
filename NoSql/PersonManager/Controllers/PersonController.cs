b742b5a03be01a4ecb2f4e1f3b1e5efaac44553c
Luka Cabraja <luka@cabraja.eu>
2022-02-11 17:08:23 +0100

    delivery-6 complete

---
+++ NoSql/PersonManager/Controllers/PersonController.cs
@@ -0,0 +1,91 @@
+﻿using PersonManager.Dal;
+using PersonManager.Models;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace PersonManager.Controllers
+{
+    public class PersonController : Controller
+    {
+        private static readonly ICosmosDbService service = CosmosDbServiceProvider.CosmosDbService;
+        // GET: Person
+        public async Task<ActionResult> Index()
+        {
+            return View(await service.GetPeopleAsync("SELECT * FROM Person"));
+        }
+
+        // GET: Person/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            return await ShowPerson(id);
+        }
+
+        private async Task<ActionResult> ShowPerson(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var person = await service.GetPersonAsync(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(person);
+        }
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Person/Create
+        [HttpPost]
+        public async Task<ActionResult> Create([Bind(Include = "FirstName, LastName, Age, Email")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                person.Id = Guid.NewGuid().ToString();
+                await service.AddPersonAsync(person);
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+
+        // GET: Person/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            return await ShowPerson(id);
+        }
+
+        // POST: Person/Edit/5
+        [HttpPost]
+        public async Task<ActionResult> Edit([Bind(Include = "Id, FirstName, LastName, Age, Email")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                await service.UpdatePersonAsync(person);
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+
+        // GET: Person/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            return await ShowPerson(id);
+        }
+
+        // POST: Person/Delete/5
+        [HttpPost]
+        public async Task<ActionResult> Delete([Bind(Include = "Id, FirstName, LastName, Age, Email")] Person person)
+        {   
+            await service.DeletePersonAsync(person.Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ NoSql/PersonManager/Dal/CosmosDbServiceProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Cosmos;
+using System.Threading.Tasks;
+
+namespace PersonManager.Dal
+{
+    public class CosmosDbServiceProvider
+    {
+        private const string Account = "https://pppk-6-people.documents.azure.com:443/";
+        private const string Key = "GQZwhAA5qTHaVBVe9iAVrCqAdLDtdbj5FarknufWlAYU1m8frp5eLKxXvDAnIcWZDDa2HMLzDNLTMClZ6qSjvA==";
+        private const string ContainerName = "People";
+        private const string DatabaseName = "People";
+
+        private static ICosmosDbService cosmosDbService;
+
+        public static ICosmosDbService CosmosDbService { get => cosmosDbService; }
+
+        public async static Task Init()
+        {
+            CosmosClient cosmosClient = new CosmosClient(Account, Key);
+            cosmosDbService = new CosmosDbService(cosmosClient, DatabaseName, ContainerName);
+            DatabaseResponse database = await cosmosClient.CreateDatabaseIfNotExistsAsync(DatabaseName);
+            await database.Database.CreateContainerIfNotExistsAsync(ContainerName, "/id");
+        }
+    }
+}
