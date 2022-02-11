4f5a396a3ef4b5d20ce64f46263a2cae1762bf5a
Luka Cabraja <luka@cabraja.eu>
2022-02-11 12:04:08 +0100

    delivery-4 complete

---
+++ ApartmentsManager/ApartmentsManager/Controllers/FileController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace ApartmentsManager.Controllers
+{
+    public class FileController : Controller
+    {
+        ~FileController()
+        {
+            if (db != null)
+            {
+                db.Dispose();
+            }
+        }
+        private readonly ModelContainer db = new ModelContainer();
+        // GET: File
+        public ActionResult Index(int id)
+        {
+            var uploadedFile = db.UploadedFiles.Find(id);
+            return File(uploadedFile.Content, uploadedFile.ContentType);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            db.UploadedFiles.Remove(db.UploadedFiles.Find(id));
+            db.SaveChanges();
+            return Redirect(Request.UrlReferrer.AbsolutePath);
+        }
+
+    }
+}
